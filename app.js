67875d9ad68a5121e33db7c6257275ae9ab8fda8
Allison Murphy <amurph@Allisons-MBP.attlocal.net>
2021-04-05 07:58:06 -0400

    clear button

--- app.js
+++ app.js
@@ -5,11 +5,18 @@
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const calc = document.querySelector(".number-pad");
 const display = document.querySelector(".screen");
-const clear = document.querySelector(".key-clear");
+// const clear = document.querySelector(".key-clear");
+const clear = ["C"];
 const lastColumn = document.querySelector(".last-column");
 
 calc.addEventListener("click", function () {
   if (numbers.indexOf(event.target.innerText) >= 0) {
     display.innerText += event.target.innerText;
+  }
+});
+
+calc.addEventListener("click", function () {
+  if (clear.indexOf(event.target.innerText) >= 0) {
+    display.innerText = "";
   }
 });
