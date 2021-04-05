b1ed68dec60d7c8c61fc65b49a3ca2946b139e23
Allison Murphy <amurph@Allisons-MBP.attlocal.net>
2021-04-05 07:52:03 -0400

    edit number querySelecto

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 // button.addEventListener("click", function () {
 //   alert(`You've clicked on button ${event.target.innerText}`);
 // });
-
-const calc = document.querySelector(".calculator");
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const calc = document.querySelector(".number-pad");
 const display = document.querySelector(".screen");
 const clear = document.querySelector(".key-clear");
+const lastColumn = document.querySelector(".last-column");
 
 calc.addEventListener("click", function () {
-  display.innerText += event.target.innerText;
+  if (numbers.indexOf(event.target.innerText) >= 0) {
+    display.innerText += event.target.innerText;
+  }
 });
