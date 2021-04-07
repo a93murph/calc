b464ad913db3b31958faf10a298c5b6bfa44d9ac
Allison Murphy <amurph@Allisons-MBP.attlocal.net>
2021-04-06 21:34:21 -0400

    calculates

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
-// const button = document.querySelector(".calculator");
-// button.addEventListener("click", function () {
-//   alert(`You've clicked on button ${event.target.innerText}`);
-// });
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const calc = document.querySelector(".number-pad");
 const display = document.querySelector(".screen");
 // const clear = document.querySelector(".key-clear");
 const clear = ["C"];
-const divide = ["÷"];
-const add = ["+"];
+const symbols = ["/", "+", "-", "*"];
+const equals = ["="];
+let equation = "";
+
+// calc.addEventListener("click", function () {
+//   if (numbers.indexOf(event.target.innerText) >= 0) {
+//     let result = parseInt((display.innerText += event.target.innerText));
+//     console.log(result);
+//   }
+// });
 
 calc.addEventListener("click", function () {
   if (numbers.indexOf(event.target.innerText) >= 0) {
-    let result = parseInt((display.innerText += event.target.innerText));
+    display.innerText += event.target.innerText;
+    equation += event.target.innerText;
+    console.log("display", display.innerText);
+    console.log("equation", equation);
+  }
+  if (symbols.indexOf(event.target.innerText) >= 0) {
+    equation += event.target.innerText;
+    display.innerText = "";
+  }
+  if (clear.indexOf(event.target.innerText) >= 0) {
+    display.innerText = "";
+    equation = "";
+  }
+  if (equals.indexOf(event.target.innerText) >= 0) {
+    let result = eval(equation);
+    display.innerText = result;
     console.log(result);
   }
 });
 
-calc.addEventListener("click", function () {
-  if (clear.indexOf(event.target.innerText) >= 0) {
-    display.innerText = "";
-  }
-});
+// for (let i = 0; i < display.length; i++) {
+//   let result = [];
+//   while (numbers.indexOf(display[i]) >= 0) {
+//     result += parseInt((display.innerText += event.target.innerText));
+//   }
+// }
 
 // calc.addEventListener("click", function () {
-//   if (add.indexOf(event.target.innerText) >= 0) {
-//     return (display + event.target.innerText)
+//   if (symbols.indexOf(event.target.innerText) >= 0) {
+//     equation += event.target.innerText;
 //   }
 // });
